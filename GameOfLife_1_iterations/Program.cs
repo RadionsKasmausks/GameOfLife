371a825295ffd00fa0ff8e87594154f2cc06ef02
radions.kasmausks <radions.kasmausks@LVAGPLTP5470>
2023-04-18 09:08:30 +0300

    Add project files.

---
+++ GameOfLife_1_iterations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Conway's Game of Life");
+
+        GameOfLife game = new GameOfLife();
+        game.Start();
+    }
+}
